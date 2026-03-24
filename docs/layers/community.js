cae2ddfbacc4fa7a72aaaa83db7d9c6f10effd3f
jpmklink-creator <jpm.klink@gmail.com>
2026-03-24 15:12:04 +0100

    Update community.js

--- docs/layers/community.js
+++ docs/layers/community.js
@@ -1,17 +1,4 @@
 var json_community = {
   "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "name": "Testkerk",
-        "url": "https://example.com",
-        "description": "test"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [4.90, 52.37]
-      }
-    }
-  ]
+  "features": []
 };
