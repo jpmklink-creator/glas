9ef610f8207b9c24631e2a36234092cfc6bc045f
monumentaal <jpm.klink@gmail.com>
2026-03-28 15:26:23 +0100

    Comment out draggable property for layer_community
    Commented out the draggable property for layer_community.

--- docs/layers/layers.js
+++ docs/layers/layers.js
@@ -130,7 +130,7 @@ var source_community = new ol.source.Vector({
 
 var layer_community = new ol.layer.Vector({
 
-// layer_community.set('draggable', true);
+
         
     declutter: false,
     source: source_community,
