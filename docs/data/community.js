b8522ac0764b1a7196c3557e79340e2ecdd62b7a
monumentaal <jpm.klink@gmail.com>
2026-03-29 15:08:41 +0200

    Update community.js with new features and coordinates

--- docs/data/community.js
+++ docs/data/community.js
@@ -1,20 +1,69 @@
-var json_community = {
+{
   "type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
       "properties": {
-        "name": "Test locatie",
-        "straat": "",
-        "huisnummer": "",
-        "postcode": "",
-        "plaats": "",
-        "url": ""
+ 	"id": 177478253333,
+        "name": "Johannes de doper",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b256/b256.shtml\" target=\"_blank\">Website</a>",
+        "description": "Kerkstraat 1, 6095ND Baexem"
       },
       "geometry": {
         "type": "Point",
-        "coordinates": [4.90, 52.37]
+        "coordinates": [
+          5.88178756817279,
+          51.229081609383144
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774782539785,
+        "name": "gemeentehuis",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b676/b676.shtml\" target=\"_blank\">Website</a>",
+        "description": "Raadhuisplen 1, 5944AH Arcen"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.179710727179911,
+          51.474383234377285
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774783425463,
+        "name": "OLv Onbevlekt Ontvangen",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b3145/b3145.shtml\" target=\"_blank\">Website</a>",
+        "description": "Aan de kerk 3, 6466AA Amstenrade"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.928179920639038,
+          50.93929756508817
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774784992052,
+        "name": "Petrus en Paulus",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b26/b26.shtml\" target=\"_blank\">Website</a>",
+        "description": "Burgemeester Gubbelsplein 1, 5944AK Arcen"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.18101277,
+          51.47535941
+        ]
       }
     }
   ]
-};
+}
