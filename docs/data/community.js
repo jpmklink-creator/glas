bfcab3ee352491910fb72b43031c06750724acb0
monumentaal <jpm.klink@gmail.com>
2026-03-29 21:41:31 +0200

    Update community.js

--- docs/data/community.js
+++ docs/data/community.js
@@ -53,5 +53,140 @@ var json_community = {
         "coordinates": [6.18101277, 51.47535941]
       }
     }
+
+
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 177478253333,
+        "name": "Johannes de doper",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b256/b256.shtml\" target=\"_blank\">Website</a>",
+        "description": "Kerkstraat 1, 6095ND Baexem"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.881724182575611,
+          51.22911778368322
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774782539785,
+        "name": "gemeentehuis",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b676/b676.shtml\" target=\"_blank\">Website</a>",
+        "description": "Raadhuisplen 1, 5944AH Arcen"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.179710727179911,
+          51.474383234377285
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774783425463,
+        "name": "OLv Onbevlekt Ontvangen",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b3145/b3145.shtml\" target=\"_blank\">Website</a>",
+        "description": "Aan de kerk 3, 6466AA Amstenrade"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.945741183836251,
+          50.948849103804406
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774784992052,
+        "name": "Petrus en Paulus",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b26/b26.shtml\" target=\"_blank\">Website</a>",
+        "description": "Burgemeester Gubbelsplein 1, 5944AK Arcen"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.18101639825796,
+          51.475399939404184
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774811573991,
+        "name": "Cosmas en Damiaan",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b230/b230.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Monseigneur Frederixplein 5, 585AM Afferden Limburg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.009563450895693,
+          51.634151741105455
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774811736794,
+        "name": "kapel OLV Moeder van smarten",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b230/b230.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Langstraat 1, 5851BA Afferden Limburg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.012420193402558,
+          51.63454748002226
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774811956596,
+        "name": "St Jozef",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b285/b285.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Pastoor Leukenstraat 2, 5966NM America"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          5.97910489,
+          51.43632674
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1774812243315,
+        "name": "Missionarissen van Marianhill",
+        "url": "<a href=\"https://www.glasmalerei-ev-web.de/pages/b241/b241.shtml\" target=\"_blank\">ðŸ”— Website</a>",
+        "description": "Kloosterstraat 8, 5944AN Arcen"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          6.181680747780236,
+          51.49808914525906
+        ]
+      }
+    }
+  ]
+
+
+
+    
   ]
 };
